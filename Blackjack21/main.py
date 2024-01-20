92da38b2780d93c63d882361b37ea70852f1a49b
Kardo Jankowitz <kardojv@gmail.com>
2024-01-20 07:56:34 +0200

    mostly working

--- Blackjack21/main.py
+++ Blackjack21/main.py
@@ -1,12 +1,6 @@
 import random
+import sys, subprocess
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-start = input("Welcome to Blackjack21! Would you like to play? Type 'y' or 'n'.\n")
-
-if start == "y":
-    game_over = False
-else:
-    game_over = True
-    print("Boo you whore")
 
 def deal_card(player, amount):
     for c in range(amount):
@@ -22,64 +16,72 @@ def calculate_score(hand):
             if card == 11:
                 hand.remove(card)
                 hand.append(1)
-                calculate_score(hand)
+                return sum(hand)
     return total
 
 
-def run_game():
-    dealer_score = calculate_score(comp_hand)
-    player_score = calculate_score(player_hand)
-
-    if dealer_score == 0:
-        print("Dealer got blackjack. Player loses!")
-        game_over = True
+def run_game(dealer_score, player_score):
+    
+    if player_score > 21 and dealer_score > 21:
+        return "Both bust. Draw"
+    elif player_score == dealer_score:
+        return "Draw"
+    elif dealer_score == 0:
+        return "Dealer got blackjack. Player loses!"
     elif player_score == 0:
-        print("Player got blackjack. Player wins!")
-        game_over = True
-    elif player_score > 21:
-        print(f"Player bust! Score {player_score} - Player loses!")
-        game_over = True
-    print(f"Player hand: {player_hand} and total {player_score}")
-    print(f"Dealer's first card is {comp_hand[0]}")
-
-def compare():
-    if calculate_score(comp_hand) > calculate_score(player_hand):
-        winner = "Dealer wins!"
-        return winner
-    elif calculate_score(comp_hand) == calculate_score(player_hand):
-        winner = "Draw"
-        return winner
+        return "Player got blackjack. Player wins!"
+    elif player_score > 21:  
+        return f"Player bust! Score {player_score} - Player loses!"
+    elif dealer_score > 21:
+        return f"Dealer bust! Score {dealer_score} - Player wins!"
+    elif player_score > dealer_score:
+        return "Player wins!"
     else:
-        winner = "Player wins!"
-        return winner
+        return "Dealer wins!"
     
 
 
-while not game_over:
+
+def play():
     comp_hand = []
     player_hand = []
     deal_card(comp_hand, 2)
     deal_card(player_hand, 2)
-    run_game()
-    dealer_score = calculate_score(comp_hand)
-    player_score = calculate_score(player_hand)
-
-    end_turn = False
-    while not end_turn:
-        deal_again = input("Does Player want another card? Type 'y' or 'n'.\n")
-        if deal_again == "y":
-            deal_card(player_hand, 1)
-            calculate_score(player_hand)
-            run_game()
-        else:
-            print("end turn")
-            end_turn = True
-
-    while dealer_score < 17:
-        deal_card(comp_hand, 1)
+    game_over = False
+    
+    while not game_over:
         dealer_score = calculate_score(comp_hand)
-        print(dealer_score)
+        player_score = calculate_score(player_hand)
+        print(f"Player hand: {player_hand} and total {player_score}\nDealer's first card is {comp_hand[0]}")
 
-    result = compare()
-    print(f"{result}\nPlayer total: {player_score}\nDealer score: {dealer_score}")
-    game_over = True
+        if dealer_score == 0 or player_score == 0 or player_score > 21:
+            game_over = True
+        else:
+            end_turn = False
+            while not end_turn:
+                deal_again = input("Does Player want another card? Type 'y' or 'n'.\n")
+                if deal_again == "y":
+                    deal_card(player_hand, 1)
+                    player_score = calculate_score(player_hand)
+                    print(f"Player hand: {player_hand} and total {player_score}")
+                else:
+                    print("Player stand")
+                    end_turn = True
+
+        while dealer_score != 0 and dealer_score < 17:
+            deal_card(comp_hand, 1)
+            dealer_score = calculate_score(comp_hand)
+            
+
+        print(f"Player total: {player_score}\nDealer total: {dealer_score}")
+        print(run_game(dealer_score, player_score))
+        game_over = True
+
+
+start = input("Welcome to Blackjack21! Would you like to play? Type 'y' or 'n'.\n")
+
+if start == "y":
+    subprocess.run("clear", shell=True)
+    play()
+else:
+    print("Boo you whore")
