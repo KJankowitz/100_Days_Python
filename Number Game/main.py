04dca37e362a2c7f814836f1fd9171cdd50c4724
Kardo Jankowitz <kardojv@gmail.com>
2024-01-23 10:22:00 +0200

    Mostly functional - next fix game loop and winning conditions

--- Number Game/main.py
+++ Number Game/main.py
@@ -1,31 +1,41 @@
 import random
+play_again = True
 
-num = random.randomint(1, 100)
-guesses = 0
-level = input("Which diffculty do you want? Type 'hard' or 'easy'./n")
+while play_again:
+    num = random.randint(1, 100)
+    guesses = 0
+    level = input("Which diffculty do you want? Type 'hard' or 'easy'.\n")
 
-if level == "hard":
-    guesses = 5
-elif level == "easy":
-    guesses = 10
+    if level == "hard":
+        guesses = 5
+    elif level == "easy":
+        guesses = 10
 
+    def check_lives():
+        if player_guess > num:
+            return - 1
+        elif player_guess < num:
+            return - 1
+        else:
+            return
 
+    def check_guess():
+        if player_guess > num:
+            return "Too high"
+        elif player_guess < num:
+            return "Too low"
+        elif player_guess == num:
+            return f"You won! The number was {num}"
+        
+    while guesses > 0:
+        player_guess = int(input("Guess the number\n"))
+        print(check_guess())
+        guesses = guesses + check_lives()
+        print(f"You have {guesses} guess(es) left.")
 
-def check_guess():
-    if player_guess > num:
-        guesses -= 1
-        return "Too high"
-    elif player_guess < num:
-        guesses -= 1
-        return "Too low"
-    elif player_guess == num:
-        return f"You won! The number was {num}"
-    
-while guesses > 0:
-    player_guess = input("Guess the number")
-    check_guess()
-    print(f"You have {guesses} guesses left.")
-
+    restart = input("Would you like to play again? Type 'y' or 'n'\n")
+    if restart == "n":
+        play_again == False
 
 
 
