dea9037abb2670a23a402374293f01381cbc3583
Kardo Jankowitz <kardojv@gmail.com>
2024-01-24 10:21:54 +0200

    Comparison functional

--- Higher Lower Game/main.py
+++ Higher Lower Game/main.py
@@ -6,7 +6,7 @@ A = data[0]
 B = ""
 
 #1 pick random dict from data list and assign to B
-n = random.randint(0, len(data))
+n = random.randint(1, len(data))
 B = data[n]
 
 #2 display all info exept followers?
@@ -14,8 +14,20 @@ print(f"Option A: {A["name"]}, a {A["description"]} from {A["country"]}")
 print(f"Option B: {B["name"]}, a {B["description"]} from {B["country"]}")
 
 #3 compare A and B and save answer?
+ANSWER = ""
+if A["follower_count"] > B["follower_count"]:
+    ANSWER = "A"
+else:
+    ANSWER = "B"
 
 #4 Ask user to guess who has more followers, A or B
+player_score = 0
+guess = input("Which one has more followers? Type 'A' or 'B'\n")
+if guess.upper() == ANSWER:
+    player_score += 1
+    print(f"Correct! Your score is {player_score}")
+else:
+    print("game over")
 
 #5 If user correct, display score and add 1
 
@@ -24,5 +36,3 @@ print(f"Option B: {B["name"]}, a {B["description"]} from {B["country"]}")
 #7 replace A with B and compare new random B. (repeat #1 - #6)
 
 #8 Add play again loop
-
-
