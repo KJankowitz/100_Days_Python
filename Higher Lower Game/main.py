575f85a75ee5ecc112c9c8f8adbcc1049bbc0b0d
Kardo Jankowitz <kardojv@gmail.com>
2024-01-24 10:52:01 +0200

    add logos and art

--- Higher Lower Game/main.py
+++ Higher Lower Game/main.py
@@ -1,5 +1,7 @@
 import random
 from game_data import data
+import sys, subprocess
+from art import logo, vs
 
 play_game = True
 A = data[0]
@@ -7,10 +9,13 @@ B = ""
 player_score = 0
 
 while play_game:
+    subprocess.run("clear", shell=True)
     n = random.randint(1, len(data))
     B = data[n]
-
+    print(logo)
+    print(f"Your current score is {player_score}")
     print(f"Option A: {A["name"]}, a {A["description"]} from {A["country"]}")
+    print(vs)
     print(f"Option B: {B["name"]}, a {B["description"]} from {B["country"]}")
 
     ANSWER = ""
@@ -29,4 +34,5 @@ while play_game:
 
     A = B
 
+#bugfix - when A and randint B is the same
 #8 Add play again loop
