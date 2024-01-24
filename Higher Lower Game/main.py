a00fcc3e6e901c5e20f107c19abf988d9b746160
Kardo Jankowitz <kardojv@gmail.com>
2024-01-24 09:54:37 +0200

    add comments

---
+++ Higher Lower Game/main.py
@@ -0,0 +1,23 @@
+import random
+
+#things to compare
+A = ""
+B = ""
+
+#1 pick random dict from data list and assign to B
+
+#2 display all info exept followers?
+
+#3 compare A and B and save answer?
+
+#4 Ask user to guess who has more followers, A or B
+
+#5 If user correct, display score and add 1
+
+#6 If user incorrect, game over
+
+#7 replace A with B and compare new random B. (repeat #1 - #6)
+
+#8 Add play again loop
+
+
