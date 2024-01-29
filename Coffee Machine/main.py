32bf2a91221604e0d470f386bdcf7fc3f26065ee
Kardo Jankowitz <kardojv@gmail.com>
2024-01-29 13:26:32 +0200

    Check resources function - espresso bug

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -26,7 +26,7 @@ MENU = {
 
 resources = {
     "water": 300,
-    "milk": 200,
+    "milk": 20,
     "coffee": 100,
 }
 
@@ -37,6 +37,20 @@ instruction = input("What would you like? (espresso/latte/cappuccino):\n")
 if instruction == "report":
     print(resources)
 # TODO: 4 Check for sufficient resources to make drink
+
+
+def check_resources(drink):
+    if resources["water"] < MENU[drink]["ingredients"]["water"]:
+        return "Not enough water"
+    elif resources["coffee"] < MENU[drink]["ingredients"]["coffee"]:
+        return "Not enough coffee"
+    if MENU[drink]["ingredients"]["milk"]:
+        if resources["milk"] < MENU[drink]["ingredients"]["milk"]:
+         return "Not enough milk"
+
+
+print(check_resources(instruction))
+
 # TODO: 5 Process coins - if sufficient resources, prompt user to insert coins
 # TODO: 6 Check successful transaction - either refunded if insufficient or added to machine as profit,
 #  or offered change
